fe124400127bb19ae489bd3d639fc4a1b7b8d636
unknown <allenpip341@gmail.com>
2023-11-10 17:21:04 +0000

    home page image resized

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import front from './images/front.jpg'
-
+import './index.css' 
 function App() {
   return (
-    <div>
-      <img src={front}/>
+
+    <div className='container1'>
+      <img className='frontimg' src={front} alt='pucks glen'/>
+      <h1>Catherine Calachi</h1>
+      <h2>Yoga instructor</h2>
     </div>
   )
 }
