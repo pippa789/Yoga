b77560ed074bc5b401b27e58f508a4fb582b93db
unknown <allenpip341@gmail.com>
2024-02-13 17:41:01 +0000

    set up array and prints to the screen in classes card

--- src/components/ClassesCard.js
+++ src/components/ClassesCard.js
@@ -1,18 +1,31 @@
 import React from 'react';
-import { useState } from 'react';
 
 
-function ClassesCard() {
-const [book, setBook]=useState(0)
-  return (
-    <>
-    <div className='classes-card'>Classes</div>
-    <button className='booking-button' onClick={() => setBook(book + 1)}>Book Now</button>
+  function ClassesCard() {
+  const Classes = 
+  [
+      {
+          id:"1",
+          title:"Mixed Yoga and Pilates",
+          time: "7am till 8am",
+      },
+      {
+          id:"2",
+          title:"Gentle Yoga",
+          time: "7:30pm till 8:30pm",
+      }
+  ]
+   return(
+  
+    <div className='classes-card'>
+    {Classes.map((Classes, index)=> (
+      <div key={index}>
+        <span>{Classes.title}</span>
+        </div> 
+       ))}
+       </div>   
+     ) 
     
-    </>
-    
-    )
-
-}
+    }
 
 export default ClassesCard
--- src/utilities/ClassData.js
+++
@@ -1,9 +0,0 @@
-const classes = {timeSlots:
-[
-    {
-        id:"1",
-        title:"Mixed Yoga and Pilates",
-        time: "7am till 8am",
-
-]
-}
